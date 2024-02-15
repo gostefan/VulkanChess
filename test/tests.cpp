9edef7215b4878445b0c3fe35800ac5400ac1534
Stefan Goetschi <github@stefan-goetschi.ch>
2024-02-15 22:47:27 +0100

    Remove library and executable from template.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
 
+namespace {
+	constexpr int value() {
+		return 4 / 2;
+	}
+}
 
-#include <VulkanChess/sample_library.hpp>
-
-
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Trivial Test", "[trivial]")
 {
-  REQUIRE(factorial(0) == 1);
-  REQUIRE(factorial(1) == 1);
-  REQUIRE(factorial(2) == 2);
-  REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(10) == 3628800);
+  REQUIRE(2 == value());
 }
